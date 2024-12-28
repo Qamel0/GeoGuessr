7b98ef77a3ff65f721c7dd59293253d79f8a7fe8
Qamel <maksostahov426@gmail.com>
2024-12-28 19:31:04 +0200

    Добавьте файлы проекта.

---
+++ SocialNetwork/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SocialNetwork.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
