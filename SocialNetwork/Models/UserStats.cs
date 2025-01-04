51558e75f98e5241cc22458281032b913b5118ae
Qamel <maksostahov426@gmail.com>
2025-01-04 20:22:21 +0200

    Added hiding menu items for small screens. I also generally corrected the page so that it fits the screen size

---
+++ SocialNetwork/Models/UserStats.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeoGuessr.Models
+{
+    public class UserStats
+    {
+        [Key]
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public int RecordDistance;
+
+    }
+}
---
+++ SocialNetwork/wwwroot/js/Scripts.js
@@ -0,0 +1,6 @@
+﻿const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
