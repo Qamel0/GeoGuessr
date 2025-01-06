e721f400a9e0301d3437432cb7d706d9b056c07a
Qamel <maksostahov426@gmail.com>
2025-01-06 20:10:25 +0200

    Minor edits

--- SocialNetwork/wwwroot/js/Scripts.js
+++ SocialNetwork/wwwroot/js/Scripts.js
@@ -1,6 +1,1 @@
-﻿const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
-});
+﻿
