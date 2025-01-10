5e8d6745830cc1a23b30959968ea450b01604b61
Qamel <maksostahov426@gmail.com>
2025-01-10 05:23:09 +0200

    Added logic for opening a menu when you click on a button that opens the same menu

---
+++ SocialNetwork/wwwroot/js/menu-toggle_button.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('menu-toggle');
+    const elements = document.querySelectorAll('.navbar');
+
+    button.addEventListener('click', () => {
+        elements.forEach(element => {
+            element.classList.toggle('navbar')
+        })
+    });
+});
